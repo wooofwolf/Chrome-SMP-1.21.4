46c5ad7bad07cb87cc7c6a148a9c75d91c5f51b3
Bryson <69586568+wooofwolf@users.noreply.github.com>
2025-01-22 13:39:13 +0800

    feat: add ArmorHudOverlay
    Created temporary display of Armor on HUD to test feasibility.

--- src/main/java/net/justwoofwolf/chromesmp/ChromeSMPClient.java
+++ src/main/java/net/justwoofwolf/chromesmp/ChromeSMPClient.java
@@ -1,10 +1,15 @@
 package net.justwoofwolf.chromesmp;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.justwoofwolf.chromesmp.client.ArmorHudOverlay;
 
 public class ChromeSMPClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        // Register armor HUD renderer
+        HudRenderCallback.EVENT.register(((drawContext, renderTickCounter) -> {
+            new ArmorHudOverlay().onHudRender(drawContext, renderTickCounter);
+        }));
     }
 }
---
+++ src/main/java/net/justwoofwolf/chromesmp/client/ArmorHudOverlay.java
@@ -0,0 +1,63 @@
+package net.justwoofwolf.chromesmp.client;
+
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.justwoofwolf.chromesmp.ChromeSMP;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
+
+public class ArmorHudOverlay implements HudRenderCallback {
+    // Texture Ids
+    private static final Identifier ARMOR_HOTBAR_TEXTURE = Identifier.of(ChromeSMP.MOD_ID,
+            "textures/hotbar.png");
+    private static final Identifier ARMOR_HOTBAR_SELECTION_TEXTURE = Identifier.of(ChromeSMP.MOD_ID,
+            "textures/hotbar_selection.png");
+
+    @Override
+    public void onHudRender(DrawContext context, RenderTickCounter renderTickCounter) {
+        int x = 0;
+        int y = 0;
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client != null) {
+            int width = client.getWindow().getScaledWidth();
+            int height = client.getWindow().getScaledHeight();
+
+            x = width / 2;
+            y = height;
+        }
+
+        context.getMatrices().push();
+        context.getMatrices().scale(1f, 1f, 1f);
+        context.getMatrices().pop();
+
+        // Render hotbar textures
+        drawTexture(context, ARMOR_HOTBAR_TEXTURE, x + 9, y - 66,  82, 22);
+        drawTexture(context, ARMOR_HOTBAR_SELECTION_TEXTURE, x + 8, y - 67, 24, 23);
+
+        // Render armor items
+        DefaultedList<ItemStack> armor = client.player.getInventory().armor;
+
+        for (int i = 0; i < 4; ++i) {
+            context.getMatrices().push();
+            context.getMatrices().translate(0f, 0f, 0f);
+            context.drawItem(armor.get(i), x + 72 - i * 20, y - 63);
+            context.getMatrices().pop();
+        }
+    }
+
+    private void drawTexture(DrawContext context, Identifier texture, int x, int y, int width, int height) {
+        context.drawTexture(
+                RenderLayer::getGuiTexturedOverlay,
+                texture,
+                x, y,
+                0f, 0f,
+                width, height,
+                width, height
+        );
+    }
+}
